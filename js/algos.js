d70713e28b4d6812b1ae31f81abc76b2d04f48c2
Lauren Mellon <laumellon@gmail.com>
2019-08-01 22:32:47 -0400

    wrote get even function

--- js/algos.js
+++ js/algos.js
@@ -2,7 +2,14 @@
 //
 //
 // loop through numbers 1 to 20
+    // for ( let i = 0; i <= 20; i ++ ) {
+    //     console.log(i);
+    // };
 
-for ( let i = 0; i <= 20; i ++ ) {
-    console.log(i);
+// loop through 0 to 200 and list even numbers
+
+for ( let i = 0; i <= 200; i ++) {
+    if ( i % 2 === 0) {
+        console.log(i);
+    }
 };
