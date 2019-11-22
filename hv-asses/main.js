98525b91cd8aeb31b13b4a2d378f6535079efa34
Lauren Mellon <laumellon@gmail.com>
2019-11-22 14:02:24 -0500

    set up function to compare two arrays and return element unique to one

--- hv-asses/main.js
+++ hv-asses/main.js
@@ -1,28 +1,49 @@
 const greeting = 'Hello World';
 //console.log(greeting);
 
-sample = [1,2,0]
-sample2 = [4,1,3,4,2]
-sample3 = [1,2,3,4,4,3,2,1,0]
+// // QUESTION 1
+// sample = [1,2,0]
+// sample2 = [4,1,3,4,2]
+// sample3 = [1,2,3,4,4,3,2,1,0]
 // const distinctValues = (seed) => {
 //     return new Set(seed).size;
 // };
+//
+// //console.log(distinctValues(sample));
+// //console.log(distinctValues(sample2));
+//
+// const distinctValues = (seed) => {
+//     let obj = {};
+//
+//     seed.forEach( num => {
+//         if(!obj[num]) {
+//             obj[num] = 0;
+//         }
+//         obj[num] ++;
+//     })
+//     return Object.keys(obj).length;
+// }
+//
+// distinctValues(sample);
 
-//console.log(distinctValues(sample));
-//console.log(distinctValues(sample2));
+// QUESTION 3
 
-const distinctValues = (seed) => {
-    let obj = {};
+let columnA = [1,2,3,4,5];
+let columnB = [1,2,3,4,5,6,7];
 
-    seed.forEach( num => {
-        if(!obj[num]) {
-            obj[num] = 0;
-        }
-        obj[num] ++;
-    })
-    console.log(obj);
-    console.log(Object.keys(obj).length);
-    return Object.keys(obj).length;
+
+const compare = (arr1, arr2) => {
+    let integers = {};
+    arr1.forEach(num => {
+        if(!integers[num]) {
+            integers[num] = 0;
+        } integers[num] ++;
+    });
+    arr2.forEach(num => {
+        if(!integers[num]) {
+            integers[num] = 0;
+        } integers[num] ++;
+    });
+    return (Object.keys(integers).find(key => integers[key] === 1));
 }
-
-distinctValues(sample);
+// compare(columnB, columnA);
